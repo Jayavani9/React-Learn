68a7bc2547890b0ee3be058618afb7495d16e7a7
Jayavani Akkiraju <51267675+Jayavani9@users.noreply.github.com>
2024-04-18 14:52:12 -0700

    Add files via upload

---
+++ example/Sample.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import TestComp from './TestComp'
+
+const Mango = () =>{
+    return (
+        <h2> Mangoes are sweet !!</h2>
+    )
+}
+
+export default function Sample() {
+  return (
+    <div>
+        <Mango />
+        <TestComp />
+    </div>
+  )
+}
+
+
